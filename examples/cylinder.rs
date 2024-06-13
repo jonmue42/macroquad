cc9ef70ada9aeea34e719a0fa9c0fcf6a740d3ed
Jonas Müller <jonmue@uni-bremen.de>
2024-06-13 16:18:33 +0200

    added example for the cylinder functions

---
+++ examples/cylinder.rs
@@ -0,0 +1,26 @@
+use macroquad::prelude::*;
+
+#[macroquad::main("Cylinder")]
+async fn main() {
+    loop {
+        clear_background(LIGHTGRAY);
+
+        set_camera(&Camera3D {
+            position: vec3(-20., 15., 0.),
+            up: vec3(0., 1., 0.),
+            target: vec3(0., 0., 0.),
+            ..Default::default()
+        });
+
+        draw_grid(30, 1., BLACK, GRAY);
+
+       draw_cylinder(vec3(0., 1., -10.), 2., 2., 5., None, DARKBLUE);
+       draw_cylinder_wires(vec3(0., 1., -5.), 2., 2., 5., None, DARKBLUE);
+       draw_cylinder(vec3(0., 1., 0.), 1., 2., 5., None, DARKBLUE);
+       draw_cylinder(vec3(0., 1., 5.), 0., 2., 5., None, DARKBLUE);
+       draw_cylinder(vec3(0., 1., 10.), 2., 0., 5., None, DARKBLUE);
+
+       next_frame().await
+
+    }
+}
