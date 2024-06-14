916cfb2c7f2b70e3c40c758fb04ed15374b3f3c6
Jonas Müller <jonmue@uni-bremen.de>
2024-06-14 10:51:36 +0200

    formatted example

--- examples/cylinder.rs
+++ examples/cylinder.rs
@@ -14,13 +14,12 @@ async fn main() {
 
         draw_grid(30, 1., BLACK, GRAY);
 
-       draw_cylinder(vec3(0., 1., -10.), 2., 2., 5., None, DARKBLUE);
-       draw_cylinder_wires(vec3(0., 1., -5.), 2., 2., 5., None, DARKBLUE);
-       draw_cylinder(vec3(0., 1., 0.), 1., 2., 5., None, DARKBLUE);
-       draw_cylinder(vec3(0., 1., 5.), 0., 2., 5., None, DARKBLUE);
-       draw_cylinder(vec3(0., 1., 10.), 2., 0., 5., None, DARKBLUE);
+        draw_cylinder(vec3(0., 1., -10.), 2., 2., 5., None, DARKBLUE);
+        draw_cylinder_wires(vec3(0., 1., -5.), 2., 2., 5., None, DARKBLUE);
+        draw_cylinder(vec3(0., 1., 0.), 1., 2., 5., None, DARKBLUE);
+        draw_cylinder(vec3(0., 1., 5.), 0., 2., 5., None, DARKBLUE);
+        draw_cylinder(vec3(0., 1., 10.), 2., 0., 5., None, DARKBLUE);
 
-       next_frame().await
-
+        next_frame().await
     }
 }
